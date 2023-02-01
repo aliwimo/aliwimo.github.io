117660be17de675e1767722001855a63c8a82e8d
Mohamed Aliwi <aliwimo@gmail.com>
2023-02-01 17:14:57 +0300

    Update build files

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/aliwimo.com/dist/',
+	base: '/',
 	server: {
 		port: 3000,
 	},
