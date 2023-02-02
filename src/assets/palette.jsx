4f8c7ed4987a79993e71678e2085de4e4927ee05
Mohamed Aliwi <aliwimo@gmail.com>
2023-02-03 01:29:10 +0300

    Add new colors

--- src/assets/palette.jsx
+++ src/assets/palette.jsx
@@ -1,15 +1,19 @@
 const palette = {
 	dark: {
 		background: {
+			lighter: "#505050",
 			light: "#494949",
 			base: "#242424",
 			dark: "#1b1b1b",
+			darker: "#161616",
 		},
 
 		foreground: {
 			base: "#fafafa",
 			dimmed: "#575757",
 		},
+
+		primary: "#1d8559"
 	},
 };
 
