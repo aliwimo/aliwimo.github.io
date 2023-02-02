846b6138af148e6fe3410afb0ae27e42131c4f6d
Mohamed Aliwi <aliwimo@gmail.com>
2023-02-02 03:24:34 +0300

    Add palette

---
+++ src/assets/palette.jsx
@@ -0,0 +1,16 @@
+const palette = {
+	dark: {
+		background: {
+			light: "#494949",
+			base: "#242424",
+			dark: "#1b1b1b",
+		},
+
+		foreground: {
+			base: "#fafafa",
+			dimmed: "#575757",
+		},
+	},
+};
+
+export default palette;
