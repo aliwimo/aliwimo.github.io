8382cb09aead61fb5882f9fdd2e729dcb5470dcc
Mohamed Aliwi <aliwimo@gmail.com>
2023-02-02 19:59:17 +0300

    Add variables file

---
+++ src/assets/variables.jsx
@@ -0,0 +1,30 @@
+const variables = {
+	transitionDuration: "300ms",
+
+	boxShadow: {
+		main: "1px 2px 4px 0px rgba(0, 0, 0, 0.1)",
+		lg: "1px 2px 8px 2px rgba(0, 0, 0, 0.2)",
+	},
+
+	borderRadius: {
+		sm: "0.25rem",
+		main: "0.5rem",
+		lg: "0.75rem",
+	},
+
+	space: {
+		xsm: "0.25rem",
+		sm: "0.5rem",
+		main: "1rem",
+		lg: "1.5rem",
+		xlg: "2rem",
+	},
+
+	breakPoint: {
+		sm: "480px",
+		md: "768px",
+		lg: "1024px",
+	}
+};
+
+export default variables;
