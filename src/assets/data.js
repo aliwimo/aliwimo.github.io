60e7e1e01e19baf11c7ce0f7df8f908a5ec9eed1
Mohamed Aliwi <aliwimo@gmail.com>
2023-02-04 22:00:25 +0300

    Update data

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,66 +1,119 @@
 export const education = [
 	{
-		id: 1,
+		title: "Master's Degree in Computer Science & Engineering",
 		date: {
 			from: 2019,
 			to: 2022
 		},
-		title: "Master's Degree in Computer Science & Engineering",
 		org: "Ondokuz Mayis University",
 		desc: "Presented Firefly Programming as a new supervised machine learning method that uses principles of symbolic regression to solve different regression-type problems."
 	},
 	{
-		id: 2,
+		title: "Bachelor's Degree in Computer Science & Engineering",
 		date: {
 			from: 2015,
 			to: 2019
 		},
-		title: "Bachelor's Degree in Computer Science & Engineering",
 		org: "Ondokuz Mayis University",
 		desc: ""
 	}
 ];
 
 export const workExperience = [
+
 	{
-		id: 1,
+		title: "Front-End React Developer",
+		date: {
+			from: "Aug 2022",
+			to: ""
+		},
+		org: "WeenFee.com",
+		desc: [
+			"Design and implement intuitive, user-friendly interfaces for web applications using React and other front-end technologies.",
+			"Collaborate with product managers, designers, and back-end developers to create high-quality, responsive user experiences.",
+			"Translate wireframes and designs into functioning code, ensuring that interfaces are consistent, accessible, and optimized for performance.",
+			"Develop and maintain reusable components and libraries, and ensure that code is well-documented and easy to maintain.",
+			"Test and debug web applications to ensure that they are fully functional and free of errors.",
+			"Stay abreast of developments in UI/UX design and front-end development, and incorporate new technologies and techniques into your work as appropriate.",
+			"Provide ongoing support and maintenance for web applications, including fixing bugs, updating interfaces, and adding new features.",
+			"Communicate effectively with team members and stakeholders, including regular updates on project progress and addressing any concerns or questions.",
+			"Continuously improve your skills and knowledge, and seek out new opportunities to expand your portfolio and experience.",
+			"Collaborate with cross-functional teams to understand end-user requirements, and help inform the design and development of new features and functionality."
+		],
+	},
+	{
+		title: "Media & Informatics Officer",
 		date: {
 			from: "Jan 2016",
 			to: ""
 		},
-		title: "Media & Informatics Officer",
 		org: "Ayadee for Relief & Development",
-		desc: ""
+		desc: [
+			"Develop and implement a comprehensive media and digital communication strategy.",
+			"Create and distribute multimedia content, such as press releases, newsletters, and social media posts, to promote our organization's activities and events.",
+			"Manage our organization's website and social media accounts, ensuring that all content is accurate, up-to-date, and consistent with our brand image.",
+			"Oversee the collection, maintenance, and analysis of data, using relevant software and tools.",
+			"Develop and maintain databases, ensuring data accuracy and security.",
+			"Collaborate with other departments to identify and address information needs, and to implement effective solutions.",
+			"Ensure that our digital platforms are user- friendly and accessible, and provide training and support to staff and stakeholders as needed.",
+			"Keep abreast of developments in the field of media and informatics, and recommend new technologies and techniques as appropriate."
+		],
 	},
 	{
-		id: 2,
+		title: "E-Learning Portal Manager",
 		date: {
 			from: "Nov 2020",
 			to: "Jan 2021"
 		},
-		title: "E-Learning Portal Manager",
 		org: "UOSSM NGO",
-		desc: ""
+		desc: [
+			"Develop and implement a strategy for the e-learning portal, in line with the organization's overall goals and objectives.",
+			"Manage the design and development of online courses, working with instructional designers and subject matter experts to ensure that content is engaging and effective.",
+			"Oversee the administration and maintenance of the e-learning portal, ensuring that it is user-friendly, accessible, and up-to-date.",
+			"Monitor student engagement and satisfaction, and make recommendations for improvements based on feedback and data analysis.",
+			"Stay abreast of developments in the field of e-learning, and incorporate new technologies and techniques into the portal as appropriate.",
+			"Manage relationships with third-party vendors, such as e-learning platform providers and content providers.",
+			"Collaborate with other departments to promote and market the e-learning portal, and to ensure that it is integrated with other aspects of the organization's operations."
+		]
 	},
 	{
-		id: 3,
+		title: "Computer Technician",
 		date: {
 			from: "Jan 2014",
 			to: "Sept 2014"
 		},
-		title: "Computer Technician",
 		org: "Akçınar Bilgisayar",
-		desc: ""
+		desc: [
+			"Install, configure, and maintain computer hardware, software, and network systems.",
+			"Troubleshoot and resolve technical problems, both in-person and remotely.",
+			"Provide technical support and training to end-users.",
+			"Maintain accurate and up-to-date records of hardware and software assets.",
+			"Install and configure operating systems, including Windows and Linux.",
+			"Install and maintain network equipment, such as routers, switches, and firewalls.",
+			"Perform routine maintenance and backups to ensure the stability and security of computer systems.",
+			"Provide after-hours support on a rotating basis, as needed.",
+			"Stay abreast of developments in the field of computer technology, and recommend new hardware and software as appropriate.",
+			"Collaborate with other IT staff to ensure the smooth and efficient operation of the organization's computer systems."
+		]
 	},
 	{
-		id: 4,
+		title: "Freelance Web Developer",
 		date: {
 			from: "Jan 2012",
 			to: ""
 		},
-		title: "Freelance Web Developer",
 		org: "Freelance",
-		desc: ""
+		desc: [
+			"Develop and maintain websites and web applications for clients, using HTML, CSS, JavaScript, and other programming languages.",
+			"Collaborate with clients to gather requirements, and translate those requirements into functional and visually appealing web solutions.",
+			"Test and debug websites and web applications to ensure that they are fully functional and free of errors.",
+			"Stay abreast of developments in web development, and incorporate new technologies and techniques into your work as appropriate.",
+			"Provide ongoing support and maintenance for websites and web applications, including fixing bugs, updating content, and adding new features.",
+			"Manage multiple projects and clients, and prioritize tasks based on deadlines and client needs.",
+			"Communicate effectively with clients, including regular updates on project progress and addressing any concerns or questions.",
+			"Manage your own time and resources effectively, and work independently with minimal supervision.",
+			"Continuously improve your skills and knowledge, and seek out new opportunities to expand your portfolio and experience."
+		]
 	}
 ];
 
