ebaff2fc482597bb79158510b75d280f6228394b
Mohamed Aliwi <aliwimo@gmail.com>
2023-02-05 02:33:29 +0300

    Add language skills

---
+++ src/components/resume/components/LangField.jsx
@@ -0,0 +1,56 @@
+import palette from "@/assets/palette";
+import styled from "styled-components";
+
+const LangField = (props) => {
+	return (
+		<Style>
+			<div className="lang">{props.lang}</div>
+			<div className="skills">
+				<div className="skill">
+					<div className="name">Reading / Writing</div>
+					<div className="meter">{props.skills.read_write}</div>
+				</div>
+				<div className="skill">
+					<div className="name">Speaking</div>
+					<div className="meter">{props.skills.speak}</div>
+				</div>
+			</div>
+		</Style>
+	);
+};
+
+const Style = styled.div`
+	width: 49%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+	
+	.lang {
+		font-size: 1.1rem;
+		font-weight: 600;
+	}
+
+	.skills {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		column-gap: 2rem;
+
+		.skill{
+			.name {
+				color: ${palette.dark.foreground.dimmed};
+				font-size: 0.8rem;
+				font-style: italic;
+			}
+
+			.meter {
+				text-transform: capitalize;
+			}
+
+		}
+	}
+
+`;
+
+export default LangField;
---
+++ src/components/resume/sections/LanguageSkills.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { languageSkills } from "@/assets/data";
+import LangField from "../components/LangField";
+import Title from "../components/Title";
+
+const LanguageSkills = () => {
+	return (
+		<>
+			<Title title="Languages" />
+			<Style>
+				{languageSkills.map((item, i) => {
+					return <LangField key={i} lang={item.lang} skills={item.skills} />;
+				})}
+			</Style>
+		</>
+	);
+};
+
+const Style = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: 0.5rem;
+`;
+
+export default LanguageSkills;
