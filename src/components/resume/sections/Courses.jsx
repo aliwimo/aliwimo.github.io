dabfa688b3827f7b0fb84c8bb768888d7a038cf8
Mohamed Aliwi <aliwimo@gmail.com>
2023-02-12 21:45:30 +0300

    Add courses section

--- src/components/resume/sections/Courses.jsx
+++ src/components/resume/sections/Courses.jsx
@@ -1,10 +1,22 @@
 import Title from "@/components/resume/components/Title";
+import Field from "@/components/resume/components/field/Field";
+import { courses } from "@/assets/data";
 
 const Courses = () => {
 	return (
 		<>
-			<Title title="Hahaha" />
-			<p>This is courses section </p>
+			<Title title="Courses & Certification" />
+			{courses.map((item, i) => {
+					return (
+						<Field
+							key={i}
+							date={item.date}
+							title={item.title}
+							org={item.org}
+							desc={item.desc}
+						/>
+					);
+				})}
 		</>
 	);
 };
